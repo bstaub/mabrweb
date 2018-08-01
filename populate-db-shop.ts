35b22c0d1d1bd09f1a61d51628a66e7940b6898f
Martin Polak <martinpolak.mail@gmail.com>
2018-08-01 16:02:26 +0200

    added productsPerOrderAssociation in populate-db-shop.ts

--- populate-db-shop.ts
+++ populate-db-shop.ts
@@ -5,7 +5,7 @@ import 'core-js/es7/reflect';
 import { database, initializeApp } from 'firebase';
 import { environment } from './src/environments/environment';
 import { dbData } from './db-data';
-import {forEach} from '@angular/router/src/utils/collection';
+
 
 
 console.log('WARNING VERY IMPORTANT - PLEASE READ THIS\n\n\n');
@@ -17,61 +17,11 @@ console.log('Any issues please contact me, Thanks, Vasco\n\n\n');
 initializeApp(environment.firebase);
 
 
-// const coursesRef = database().ref('courses');
-// const lessonsRef = database().ref('lessons');
 const productsRef = database().ref('products');
 const ordersRef = database().ref('orders');
-const association = database().ref('productsPerOrder');
 // const usersRef = database().ref('users');
 
 
-/*
-dbData.courses.forEach(course => {
-
-  console.log('adding course', course.url);
-
-  const courseRef = coursesRef.push({
-    url: course.url,
-    description: course.description,
-    iconUrl: course.iconUrl,
-    courseListIcon: course.courseListIcon,
-    longDescription: course.longDescription
-  });
-
-  const lessonKeysPerCourse = [];
-
-  course.lessons.forEach((lesson: any) => {
-
-    console.log('adding lesson ', lesson.url);
-
-    lessonKeysPerCourse.push(lessonsRef.push({
-      description: lesson.description,
-      duration: lesson.duration,
-      url: lesson.url,
-      tags: lesson.tags,
-      videoUrl: lesson.videoUrl || null,
-      longDescription: lesson.longDescription,
-      courseId: courseRef.key
-    }).key);
-
-  });
-
-
-  const association = database().ref('lessonsPerCourse');
-
-  const lessonsPerCourse = association.child(courseRef.key);
-
-  lessonKeysPerCourse.forEach(lessonKey => {
-    console.log('adding lesson to course ');
-
-    const lessonCourseAssociation = lessonsPerCourse.child(lessonKey);
-
-    lessonCourseAssociation.set(true);
-  });
-
-
-});
-*/
 
 const productKeys = [];
 const orderKeys = [];
@@ -123,41 +73,4 @@ orderKeys.forEach((orderKey) => {
 });
 
 
-/*
-dbData.orders.forEach(order => {
-
-  console.log('adding order', order.name);
-
-  const orderRef = ordersRef.push({
-    userId: order.userId,
-    date: order.date,
-    status: order.status,
-    totalValue: order.totalValue
-  });
-
-
-});
-
-dbData.users.forEach(user => {
-
-  console.log('adding user', user.name);
-
-  const userRef = usersRef.push({
-    lastName: user.name,
-    firstName: user.description,
-    email: user.price,
-    natel: user.picture,
-    location: user.createdDate,
-    zip: user.createdDate,
-    country: user.createdDate,
-    role: user.createdDate,
-    status: user.createdDate,
-    createdDate: user.createdDate,
-    lastLoginDate: user.createdDate,
-    validated: user.createdDate
-  });
-
-
-});
-*/
 

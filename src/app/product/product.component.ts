955d1e6aa6bf1c6a2cce9340c0e2d1cab69a698d
Bruno Staub <bruno.staub@gmail.com>
2018-09-28 17:48:37 +0200

    basket icon modal testing

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ProductService} from './shared/product.service';
+import {LocalStorageService} from '../shared/local-storage.service';
 
 @Component({
   selector: 'app-product',
@@ -10,8 +11,13 @@ import {ProductService} from './shared/product.service';
 })
 export class ProductComponent implements OnInit {
 
+  shoppingCartContent: string;
+
   basket = [];
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService,
+              private localStorageService: LocalStorageService
+
+  ) { }
 
   ngOnInit() {
 
@@ -19,6 +25,27 @@ export class ProductComponent implements OnInit {
 
   getBasket() {
     this.basket = this.productService.getbasket();
+  }
+
+  getProductsFromLocalStorage() {
+    const itemsForBasket = this.localStorageService.getData('products');
+
+    itemsForBasket.forEach(function(item) {
+      const row = document.createElement('tr');
+      row.innerHTML = `
+               <tr>
+                    <td>
+                         <img src="${item.image}" width=100>
+                    </td>
+                    <td>${item.title}</td>
+                    <td>${item.price}</td>
+                    <td>
+                         <a href="#" class="remove" data-id="${item.id}">X</a>
+                    </td>
+               </tr>
+          `;
+      this.shoppingCartContent.appendChild(row);
+    });
   }
 
 }

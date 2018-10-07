4638f6f12f9d467a0cd956c74817bbc7c95231c7
Martin Polak <martinpolak.mail@gmail.com>
2018-10-07 17:53:25 +0200

    CustomerAddress Form added

--- src/app/checkout/checkout-customerdata/checkout-customerdata.component.ts
+++ src/app/checkout/checkout-customerdata/checkout-customerdata.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout-enterdata',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class CheckoutCustomerdataComponent implements OnInit {
+  CustomerAddressForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {
+    this.initCustomerAddressFormGroup();
+
+  }
+
+
+  private initCustomerAddressFormGroup() {
+
+    this.CustomerAddressForm = new FormGroup({
+      firstname: new FormControl(null),
+      lasttname: new FormControl(null),
+      addresss: new FormControl(null),
+      zip: new FormControl(null),
+      city: new FormControl(null),
+      country: new FormControl(null),
+      email: new FormControl(null),
+      phone: new FormControl(null)
+    });
+  }
 }

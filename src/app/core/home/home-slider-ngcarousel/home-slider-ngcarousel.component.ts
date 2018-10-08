9d239a27f8ef55d54944ad31d9a1698c0118d8ad
Bruno Staub <bruno.staub@gmail.com>
2018-10-08 12:32:42 +0200

    sliderdata object array erstellt

--- src/app/core/home/home-slider-ngcarousel/home-slider-ngcarousel.component.ts
+++ src/app/core/home/home-slider-ngcarousel/home-slider-ngcarousel.component.ts
@@ -7,7 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeSliderNgcarouselComponent implements OnInit {
 
-  images = [1, 2, 3].map(() => `https://picsum.photos/1200/500?random&t=${Math.random()}`);
+  // images = [1, 2, 3].map(() => `https://picsum.photos/1200/500?random&t=${Math.random()}`
+  public sliderdata: { image: any, title: string, text: string  }[] = [
+    {
+      image: `https://picsum.photos/1200/500?random&t=${Math.random()}`,
+      title: 'Produkt1',
+      text: 'Hier der Produktbeschreib1'
+    },
+    {
+      image: `https://picsum.photos/1200/500?random&t=${Math.random()}`,
+      title: 'Produkt2',
+      text: 'Hier der Produktbeschreib2'
+    },
+    {
+      image: `https://picsum.photos/1200/500?random&t=${Math.random()}`,
+      title: 'Produkt3',
+      text: 'Hier der Produktbeschreib3'
+    },
+  ];
 
   constructor() { }
 

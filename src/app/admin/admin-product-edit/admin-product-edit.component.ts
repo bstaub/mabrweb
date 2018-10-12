7d295c8da97db86a58b41c8f922b2b7c75f3d461
Bruno Staub <bruno.staub@gmail.com>
2018-10-12 02:37:37 +0200

    Testing NG Prime Dropdown, Selektboxen und Image Upload Works!

--- src/app/admin/admin-product-edit/admin-product-edit.component.ts
+++ src/app/admin/admin-product-edit/admin-product-edit.component.ts
@@ -1,18 +1,13 @@
-import { AfterContentChecked, AfterContentInit, Component, OnChanges, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import * as firebase from 'firebase';
 import { ProductService } from '../../product/shared/product.service';
 import { ProductFirestoreService } from '../../product/shared/product-firestore.service';
 import { StorageService } from '../../shared/storage.service';
 import { ProductCategoryService } from '../../product/shared/product-category.service';
-import { Observable, Subscription } from 'rxjs';
+import { Observable } from 'rxjs';
 import { ProductCategory } from '../../product/product-category.model';
-import { first, tap } from 'rxjs/operators';
+import { Product } from '../../models/product.model';
 
-
-interface City {
-  name: string;
-  code: string;
-}
 
 @Component({
   selector: 'app-admin-product-edit',
@@ -20,87 +15,53 @@ interface City {
   styleUrls: ['./admin-product-edit.component.css']
 })
 export class AdminProductEditComponent implements OnInit {
-  cities: City[];
-  selectedCity: City;
+  product: Product = new Product();
+  submitted = false;
+  createdDate: string;
   categories: Observable<ProductCategory[]>;
-  // categories: Promise<ProductCategory[]>;
-  categorySubscription: Subscription;
-  categoryArray = [];
   selectedCategory: ProductCategory;
-
-
-  selectedValues: string[] = ['val1', 'val2', 'val3'];
-
-
+  // selectedCategory: string = '';
   image: any;
+  discount: boolean = false;
+  newProduct: boolean = false;
+  bestRated: boolean = false;
+  // selectedValues: string[] = [];  // ['discount', 'newProduct', 'bestRated'];
 
   constructor(
     private productService: ProductService,
     private productFirestoreService: ProductFirestoreService,
     private storageService: StorageService,
     private productCategory: ProductCategoryService,
-  ) {
-
-    this.cities = [
-      {name: 'New York', code: 'NY'},
-      {name: 'Rome', code: 'RM'},
-      {name: 'London', code: 'LDN'},
-      {name: 'Istanbul', code: 'IST'},
-      {name: 'Paris', code: 'PRS'}
-    ];
-
-    /*
-    this.categoryArray = [
-      {'name': 'Computer', 'code': 'Alles über Computer'},
-      {'name': 'Fussball', 'code': 'Hier die Beeschreibung der Kategorie'}
-    ];
-    */
-
-
-
-
-  }
+  ) { }
 
 
   ngOnInit() {
     this.categories = this.productCategory.getCategories();
-
-    /*
-    // Observable Way
-    this.categories = this.productCategory.getCategories().pipe(
-      first(),
-      tap( val => console.log('inside: ', val))
-    );
-    this.categorySubscription = this.categories.subscribe(
-
-
-      val => val.forEach( x => {
-              this.categoryArray.push({name: x.name, code: x.description});
-          })
-    );
-
-    console.log(this.categoryArray);
-    console.log(this.cities);
-    */
-
-
-    /*
-    // Promise Way
-    this.categories = this.productCategory.getCategories().pipe(
-      first(),
-      // tap( val => console.log('inside: ', val))
-    ).toPromise();
-    this.categories.then(
-      val => val.forEach(x => {
-        this.categoryArray.push({name: x.name, code: x.description});
-      })
-    );
-    console.log(this.categoryArray);
-    console.log(this.cities);
-    */
-
   }
 
+  onSubmit() {
+    this.submitted = true;
+    // this.save();  // Realtime DB see save above
+    // this.userService.getCurrentUserId();
+
+    if (!this.image) {  // add default noImage Pic, when no image is choosen..
+      this.image = 'https://firebasestorage.googleapis.com/v0/b/mabrweb-e6503.appspot.com/o/mvi9oepg?alt=media&token=69801fdc-bbb0-4e19-84e3-e87b5615ca0b';
+    }
+
+    const productObj = Object.assign({
+        key: this.productFirestoreService.getPushKey(),
+        image: this.image,
+        // productCategory: this.selectedCategory,
+        productCategory: this.selectedCategory.name,
+        createdDate: firebase.firestore.FieldValue.serverTimestamp(),
+        discount: this.discount,
+        newProduct: this.newProduct,
+        bestRated: this.bestRated,
+      },
+      this.product);
+
+    this.productFirestoreService.addProduct(productObj);
+  }
 
   onFileSelection($event) {
     this.storageService.upload($event)
@@ -108,15 +69,6 @@ export class AdminProductEditComponent implements OnInit {
 
         uploadSnapshot.ref.getDownloadURL().then((downloadURL) => {
           this.image = downloadURL;
-
-          /*
-          // update Image
-          const data: Product = {
-            key: this.product.key,
-            downloadUrl: downloadURL,
-          };
-          this.productFirestoreService.setProduct(data);
-          */
 
         });
 
--- src/app/models/product.model.ts
+++ src/app/models/product.model.ts
@@ -11,6 +11,6 @@ export class Product {
   productCategory?: string;
   discount?: boolean;
   newProduct?: boolean;
-  bestRated?: number;
+  bestRated?: boolean;
 }
 

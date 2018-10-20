3536ea4efdb92f50ff5902200ea1071c525651ac
Bruno Staub <bruno.staub@gmail.com>
2018-10-20 10:16:20 +0200

    home slider full width, comment out home styles on home

--- src/app/core/shared/slider.service.ts
+++ src/app/core/shared/slider.service.ts
@@ -13,19 +13,19 @@ export class SliderService {
 
     this.slider = [
       {
-        image: `https://picsum.photos/2500/500?random&t=${Math.random()}`,
+        image: `https://picsum.photos/2560/500?random&t=${Math.random()}`,
         title: 'Ipad Air',
         text: 'Hier das neue IPAD Air',
         link: 'http://localhost:4200/produkte/detail/eVR3Tj9bRLIAsjD9CrQ6',
       },
       {
-        image: `https://picsum.photos/2500/500?random&t=${Math.random()}`,
+        image: `https://picsum.photos/2560/500?random&t=${Math.random()}`,
         title: 'Winter Jacke',
         text: 'Hier der Produktbeschreib2 Winter Jacke',
         link: 'http://localhost:4200/produkte/detail/dMCPQ1NYxKGCa5yvvaII',
       },
       {
-        image: `https://picsum.photos/2500/500?random&t=${Math.random()}`,
+        image: `https://picsum.photos/2560/500?random&t=${Math.random()}`,
         title: 'Salewa Schuhe',
         text: 'Hier der Produktbeschreib3',
         link: 'https://www.salewa.com/de-ch',

64a55f997a8c917a021cdcd0af318980c4033137
Bruno Staub <bruno.staub@gmail.com>
2018-10-20 12:01:33 +0200

    search suggest feature testing

--- src/app/core/page-title/page-title.component.ts
+++ src/app/core/page-title/page-title.component.ts
@@ -1,11 +1,36 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ProductFirestoreService } from '../../product/shared/product-firestore.service';
 
 @Component({
   selector: 'app-page-title',
   templateUrl: './page-title.component.html',
   styleUrls: ['./page-title.component.scss']
 })
-export class PageTitleComponent {
+export class PageTitleComponent implements OnInit {
+  filteredProducts: any[] | number;
+
   @Input() public title: string;
   @Input() public children: {title: string, link: string}[];
+  @Input() public homepage: boolean = false;
+
+  constructor(private productFireStoreService: ProductFirestoreService) {
+  }
+
+
+  ngOnInit() {
+    // RxJS BehaviorSubject start
+    this.productFireStoreService.currentMessage.subscribe(message => {
+      if (message !== 0) {
+        this.filteredProducts = message;
+      } else {
+        this.filteredProducts = null;
+      }
+    });
+    // RxJS BehaviorSubject end
+  }
+
+  closeSuggest() {
+    this.filteredProducts = 0;
+  }
+
 }

7ea133652c593d21de7f8c6d860ee4ec976d105f
Bruno Staub <bruno.staub@gmail.com>
2018-10-28 19:34:31 +0100

    Delete Logik mit Alertify added, Pushkey in Item hinzugefügt, RWD Tabke Styling

--- src/app/admin/admin-product-edit-category/admin-product-edit-category.component.ts
+++ src/app/admin/admin-product-edit-category/admin-product-edit-category.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ProductCategoryService } from '../../product/shared/product-category.service';
 import { Observable } from 'rxjs';
 import { ProductCategory } from '../../models/product-category.model';
+import { AlertifyService } from '../../shared/alertify.service';
 
 @Component({
   selector: 'app-admin-product-edit-category',
@@ -12,12 +13,20 @@ export class AdminProductEditCategoryComponent implements OnInit {
 
   categories: Observable<ProductCategory[]>;
 
-  constructor(private productCategoryService: ProductCategoryService) {
+  constructor(private productCategoryService: ProductCategoryService,
+              private alertifyService: AlertifyService
+  ) {
   }
 
   ngOnInit() {
     this.categories = this.productCategoryService.getCategories();
   }
 
+  onDeletItem(id: string) {
+    this.alertifyService.confirm('Wollen Sie diese Kategorie wirklich löschen?', () => {
+    this.alertifyService.success('Die Kategorie wurde erfolgreich gelöscht.');
+    this.productCategoryService.deleteCategory(id);
+    });
+  }
 
 }
--- src/app/product/shared/product-category.service.ts
+++ src/app/product/shared/product-category.service.ts
@@ -30,24 +30,49 @@ export class ProductCategoryService {
     return this.categories;
   }
 
+  addCategory(category: ProductCategory) {
+    // this.productCollection.add(product);
+    this.productCategoryCollection.add(category).then((docRef) => {
+      console.log('Product written with ID: ', docRef.id);
+      const NewCategoryKey: ProductCategory = {
+        id: docRef.id,
+      };
+      this.setCategory(NewCategoryKey);
+    }).catch(function (error) {
+      console.error('Error adding document: ', error);
+    });
+  }
+
+  deleteCategory(id: string) {
+    this.categoryDoc = this.afs.doc(`categories/${id}`);
+    console.log('kat in service:', id);
+    this.categoryDoc.delete();
+  }
+
+  setCategory(category: ProductCategory) {
+    this.categoryDoc = this.afs.doc(`categories/${category.id}`);
+    return this.categoryDoc.set(category, {merge: true});
+  }
+
+  getPushKey() {
+    return this.afs.createId();
+  }
+
   getCategory(id) {
     this.categoryDoc = this.afs.doc(`categories/${id}`);
     return this.categoryDoc;
   }
 
+  updateProduct(categoryKey, category: ProductCategory) {
+    this.categoryDoc = this.afs.doc(`categories/${categoryKey}`);
+    this.categoryDoc.update(category);
+  }
+
+  /*
   addCategory(category: ProductCategory) {
-    this.productCategoryCollection.add(category);
+  this.productCategoryCollection.add(category);
   }
-
-  deleteCategory(id: string) {
-    this.categoryDoc = this.afs.doc(`categories/${id}`);
-    this.categoryDoc.delete();
-  }
-
-  setCategory(category: ProductCategory) {
-    this.categoryDoc = this.afs.doc(`products/${category.id}`);
-    return this.categoryDoc.set(category, {merge: true});
-  }
+  */
 
   /*
   updateCategory(category: ProductCategory) {
@@ -55,14 +80,4 @@ export class ProductCategoryService {
     this.categoryDoc.update(category);
   }
   */
-
-  updateProduct(categoryKey, category: ProductCategory) {
-    this.categoryDoc = this.afs.doc(`categories/${categoryKey}`);
-    this.categoryDoc.update(category);
-  }
-
-  getPushKey() {
-    return this.afs.createId();
-  }
-
 }

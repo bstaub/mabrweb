86add35ca53af4ce3921c359b660f9d45892c254
Bruno Staub <bruno.staub@gmail.com>
2018-11-02 12:26:55 +0100

    RWD Fixes, OffCanvas Menu, Admin Guard, Search, Breadcrumb

--- src/app/core/home/off-canvas/off-canvas.component.ts
+++ src/app/core/home/off-canvas/off-canvas.component.ts
@@ -1,4 +1,9 @@
 import { Component, EventEmitter, Input, OnChanges, OnInit, Output } from '@angular/core';
+import { AuthService } from '../../../user/shared/auth.service';
+import { Order } from '../../../models/order.model';
+import { LocalStorageService } from '../../../shared/local-storage.service';
+import { OrderFlyoutService } from '../../shared/order-flyout-service';
+import { AdminGuard } from '../../../user/guards/admin.guard';
 
 @Component({
   selector: 'app-off-canvas',
@@ -7,14 +12,30 @@ import { Component, EventEmitter, Input, OnChanges, OnInit, Output } from '@angu
 })
 export class OffCanvasComponent implements OnInit, OnChanges {
 
+  isLoggedIn: boolean;
+  isAdmin = false;
+  loggedInUser: string;
   @Input() changeCanvasState;  // get open click state from navbar
   @Output() closeNavState = new EventEmitter();
 
-  constructor() {
+  constructor(private authService: AuthService,
+              private localStorageService: LocalStorageService,
+              private orderFlyoutService: OrderFlyoutService,
+  ) {
   }
 
   ngOnInit() {
-
+    this.authService.user$.subscribe(
+      auth => {
+        if (auth && auth.emailVerified) {
+          this.isLoggedIn = true;
+          this.loggedInUser = auth.email;
+          this.isAdmin = auth.roles.admin;
+        } else {
+          this.isLoggedIn = false;
+        }
+      }
+    );
   }
 
   ngOnChanges() {
@@ -34,6 +55,16 @@ export class OffCanvasComponent implements OnInit, OnChanges {
     this.closeNavState.emit();
     document.getElementById('mySidenav').style.width = '0';
     document.getElementById('main').style.marginLeft = '0';
+  }
+
+  logout() {
+    this.closeNavState.emit();
+    document.getElementById('mySidenav').style.width = '0';
+    document.getElementById('main').style.marginLeft = '0';
+    this.authService.logout();
+    this.isLoggedIn = false;
+    this.localStorageService.destroyLocalStorage('products');
+    this.orderFlyoutService.refreshOrderFlyout(this.localStorageService.getData('products'), new Order());
   }
 
 }
--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -8,7 +8,7 @@ import { User } from '../../models/user';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
 

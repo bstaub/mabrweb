4bd535dd1b9c4daa87982f4f14d60bffbbd0fa41
Bruno Staub <bruno.staub@gmail.com>
2018-11-02 15:22:03 +0100

    css fixing filter

--- src/app/core/home/off-canvas/off-canvas.component.ts
+++ src/app/core/home/off-canvas/off-canvas.component.ts
@@ -49,14 +49,14 @@ export class OffCanvasComponent implements OnInit, OnChanges, OnDestroy {
   /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
   openNav() {
     document.getElementById('mySidenav').style.width = '250px';
-    document.getElementById('main').style.marginLeft = '250px';
+    // document.getElementById('main').style.marginLeft = '250px';
   }
 
   /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
   closeNav() {
     this.closeNavState.emit();
     document.getElementById('mySidenav').style.width = '0';
-    document.getElementById('main').style.marginLeft = '0';
+    // document.getElementById('main').style.marginLeft = '0';
   }
 
   logout() {
